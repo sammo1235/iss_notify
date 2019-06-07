767cc3aefabc36a392f24a62c0ada411db1e92d0
sammo1235 <samrhysperry93@hotmail.co.uk>
2019-06-07 18:57:02 +0100

    add sendgrid mailer

---
+++ app/models/sendgrid.rb
@@ -0,0 +1,14 @@
+require 'sendgrid-ruby'
+include SendGrid
+
+from Email.new(email: 'test@example.com')
+subject = 'The ISS is above you!'
+to = Email.new(email: 'samrhysperry93@hotmail.co.uk')
+content = Content.new(type: 'text/plain', value: 'Look outside!')
+mail = Mail.new(from, subject, to, content)
+
+sg = SendGrip::API.new(api_key: ENV['SENDGRID_API_KEY'])
+response = sg.client.mail._('send').post(request_body: mail.to_json)
+puts response.status_code
+puts response.body
+puts response.headers
