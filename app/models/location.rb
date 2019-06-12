e013e85a1aa8c0789e668ef8dd3129619f4cfeaf
sammo1235 <samrhysperry93@hotmail.co.uk>
2019-06-12 22:00:31 +0100

    refactor to one location class

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,7 +1,25 @@
 class Location
+  attr_accessor :latitude, :longitude
   include HTTParty
-  
+
   def initialize(uri)
-    @location = self.class.get(uri)
+    location = self.class.get(uri)
+    @latitude = deep_find(location, 'latitude').to_f
+    @longitude = deep_find(location, 'longitude').to_f
+  end
+  
+  def deep_find(obj, key)
+    if obj.respond_to?(:key?) && obj.key?(key)
+      obj[key]
+    elsif obj.respond_to?(:each)
+      r = nil
+      obj.find { |*a| r = deep_find(a.last, key) }
+      r
+    end
+  end
+  
+  def in_range?(other)
+    iss_lat = other.latitude; iss_long = other.longitude
+    latitude.between?(iss_lat - 5, iss_lat + 5) && longitude.between?(iss_long - 5, iss_long + 5)
   end
 end
--- lib/tasks/scheduler.rake
+++ lib/tasks/scheduler.rake
@@ -1,6 +1,7 @@
 desc "Task for Heroku scheduler: email when in_range? == true"
 task :in_range? => :environment do
-  if MyLocation.new.in_range?(IssLocation.new)
+  if Location.new(Rails.application.credentials.aws[:my_location_key])
+    .in_range?(Location.new('http://api.open-notify.org/iss-now.json'))
     IssNotifyMailer.send_iss_email(Rails.application.credentials.aws[:email]).deliver_now
     puts "Sending email"
   else
