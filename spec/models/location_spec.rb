8b90877c172a681591cdfdbaba32fbbd022d9c4c
sammo1235 <samrhysperry93@hotmail.co.uk>
2019-06-12 22:11:35 +0100

    delete unnecessary tests

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -1,16 +1,18 @@
 require 'rails_helper'
 
-RSpec.describe MyLocation, type: :model do
-  let(:my_loc) { MyLocation.new }
-  let(:iss_loc) { IssLocation.new }
+RSpec.describe Location, type: :model do
+  let(:my_loc) { Location.new(Rails.application.credentials.aws[:my_location_key]) }
+  let(:iss_loc) { Location.new('http://api.open-notify.org/iss-now.json') }
   
   describe "#latitude and #longitude" do
     it "gives a latitude within expected values" do
       expect(my_loc.latitude).to be_between(-90, 90).inclusive
+      expect(iss_loc.latitude).to be_between(-90, 90).inclusive
     end
       
     it "gives a longitude within expected values" do
       expect(my_loc.longitude).to be_between(-180, 180).inclusive
+      expect(iss_loc.longitude).to be_between(-180, 180).inclusive
     end
   end
   
@@ -18,8 +20,8 @@ RSpec.describe MyLocation, type: :model do
     before do
       my_loc.latitude = 12.312
       my_loc.longitude = 0.414
-      iss_loc.iss_lat = 11.123
-      iss_loc.iss_long = 0.321
+      iss_loc.latitude = 11.123
+      iss_loc.longitude = 0.321
     end
     
     it 'returns true when both coordinates are within 5 degrees' do
@@ -31,8 +33,8 @@ RSpec.describe MyLocation, type: :model do
     before do
       my_loc.latitude = 89.312
       my_loc.longitude = 100.414
-      iss_loc.iss_lat = 11.123
-      iss_loc.iss_long = 0.321
+      iss_loc.latitude = 11.123
+      iss_loc.longitude = 0.321
     end
     
     it 'returns false when both coordinates are within 5 degrees' do
